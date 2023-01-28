fe00802fa2d68c3e1a61499f59fb4dca1a5dac4e
NikiTricky <niki.sto2010@gmail.com>
2023-01-28 22:46:36 +0200

    Add rendering

--- main.py
+++ main.py
@@ -3,27 +3,14 @@ import chess.engine
 import chess.pgn
 from tqdm import tqdm
 
-PATH_TO_ENGINE= "/path/to/engine"
-engine = chess.engine.SimpleEngine.popen_uci(PATH_TO_ENGINE)
+engine = chess.engine.SimpleEngine.popen_uci('/home/niki/Documents/Code/Python/blunderbot/stockfish-ubuntu-20.04-x86-64')
 
 # THIS GOVERNS HOW LONG A MOVE CALCULATION TAKES
 TIME = 10 # Time in seconds a move calculation should take
 TIME_PER_MOVE = None # Time per individual move calculation
 DEPTH = None # Depth at witch the engine should search at
 
-def checkList(lst):
-    ele = lst[0]
-    chk = True
-
-    # Comparing each element with first item
-    for item in lst:
-        if ele != item:
-            chk = False
-            break
-    return chk
-
-def column(matrix, i):
-    return [row[i] for row in matrix]
+RENDER_BOARD = "board.svg" # Render a board after every move. Good for troubleshooting. Set to None or the path to the board to be rendered.
 
 def get_worst_move(board, engine, t=10, tm=None, d=None):
     moves = []
@@ -43,23 +30,17 @@ def get_worst_move(board, engine, t=10, tm=None, d=None):
         score_pov = score.pov(board.turn)
         if score_pov.score():
             move_score = score_pov.score()
-            moves.append([board.lan(el), move_score, analisys.get("depth"), 0])
+            moves.append([el, move_score, analisys.get("depth"), 0])
         elif score_pov.mate():
             move_score = abs(int(score_pov.mate()))
-            mate.append([board.lan(el), move_score, analisys.get("depth"), 1])
+            mate.append([el, move_score, analisys.get("depth"), 1])
         else:
-            moves.append([board.lan(el), 1e6, analisys.get("depth"), 0])
+            moves.append([el, 1e6, analisys.get("depth"), 0])
 
     if mate:
         mate = sorted(mate, key=lambda x: x[1])
         return mate[-1]
-
-    if checkList(column(moves, 1)):
-        move = random.choice(moves)
-        move.append(moves)
-        return move
     moves = sorted(moves, key=lambda x: x[1])
-    moves[0].append(moves)
     return moves[0]
 
 board = chess.Board()
@@ -79,9 +60,9 @@ while not (board.is_checkmate() or board.is_stalemate()):
     if player_color == to_move:
         worst_move = get_worst_move(board, engine, t=TIME, tm=TIME_PER_MOVE, d=DEPTH)
         if worst_move[3]: # Check if a move is mate
-            print(f"Move: {worst_move[0]} (Mate in {worst_move[1]}, Depth: {worst_move[2]})")
+            print(f"Move: {board.lan(worst_move[0])} (Mate in {worst_move[1]}, Depth: {worst_move[2]})")
         else:
-            print(f"Move: {worst_move[0]} (Score: {worst_move[1]}, Depth: {worst_move[2]})")
+            print(f"Move: {board.lan(worst_move[0])} (Score: {worst_move[1]}, Depth: {worst_move[2]})")
 
     if to_move:
         move = input("White move: ")
@@ -105,6 +86,10 @@ while not (board.is_checkmate() or board.is_stalemate()):
         # raise e
         print("Invalid move (error code). FEN code:", board.fen())
         continue
+
+    if RENDER_BOARD:
+        with open(RENDER_BOARD, "w") as f:
+            f.write(chess.svg.board(board, fill=dict.fromkeys([move.from_square, move.to_square], "#30ed26aa")))
 
     to_move = int(not to_move)
 
